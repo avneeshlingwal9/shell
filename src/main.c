2084f1aec341ec7033995c6a3d24a5cd461897f6
avneeshlingwal9 <avaneeshlingwal@gmail.com>
2025-06-08 16:56:29 +0530

    Add type

--- src/main.c
+++ src/main.c
@@ -30,8 +30,23 @@ int main(int argc, char *argv[]) {
 
     char* command; 
     command = strtok(inputProcess , " ");
-    if(strcmp(command , "exit") == 0){
-      break;
+    if(strcmp(command, "type") == 0){
+
+      command = strtok(NULL , " ");
+
+      if(strcmp(command , "exit") == 0 || strcmp(command , "type") == 0 || strcmp(command , "echo") == 0){
+        printf("%s is a shell builtin\n" , command);
+      }
+      else{
+
+        printf("%s: not found\n" , command);
+
+      }
+
+    }
+
+    else if(strcmp(command , "exit") == 0){
+      exit(0);
     }
 
     else if(strcmp(command , "echo") == 0){
